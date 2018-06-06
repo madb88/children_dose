222b969593150f67f94558a7dbd90061206c42a3
Jakub <kaminskiqba@gmail.com>
2018-06-06 21:34:27 +0200

    Created first look of app. Implemented algo

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,156 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+ @override
+   Widget build(BuildContext context) {
+     // TODO: implement build
+     return MaterialApp(
+       title:'Children Dose',
+       theme: ThemeData(
+         primarySwatch: Colors.indigo,
+       ),
+       home: CalculateDoseForm(),
+     );
+   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class CalculateDoseForm extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  CalculateDoseFormState createState() => CalculateDoseFormState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class CalculateDoseFormState extends State<CalculateDoseForm> {
+  var 
+  _a = 0.0, 
+  _b = 0.0, 
+  _c = 0.0, 
+  _d = 0.0, 
+  _e = 0.0,
+  score = 0.0,
+  _f = 0.0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final TextEditingController t1 = new TextEditingController(text: "0");
+  final TextEditingController t2 = new TextEditingController(text: "0");
+  final TextEditingController t3 = new TextEditingController(text: "0");
+  final TextEditingController t4 = new TextEditingController(text: "0");
+  final TextEditingController t5 = new TextEditingController(text: "0");
+
+  void onPressed(){
+      setState(() {
+        _a = double.parse(t1.text);
+        _b = double.parse(t2.text);
+        _c = double.parse(t3.text);
+        _d = double.parse(t4.text);
+        _e = double.parse(t5.text);
+        _f = (_c * _d)/_e;   
+        score = (_f * _b) / _a;       
+      }); 
+    }
+
+  final formKey = GlobalKey<FormState>();
+  final mainKey = GlobalKey<ScaffoldState>();
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+    Widget build(BuildContext context) {
+      return Scaffold(
+        key: mainKey,
+        appBar: AppBar(title: Text("Calculate Form")),
+        body: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: <Widget>[
+                new Text(
+                  "Zawartosc substancji czynnej w ml leku",
+                  style: new TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple,
+                  ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Custom 1",
+                    hintText: "Enter Number in mg",
+                  ),
+                  controller: t1,
+                  keyboardType: TextInputType.number,
+                  
+                  ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Custom 2",
+                    hintText: "Enter Number",
+              
+                  ),
+                  controller: t2,
+                  keyboardType: TextInputType.number,
+                
+                  ),
+                  new Text(
+                  "Docelowa Dawka",
+                  style: new TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple,
+                  ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Custom 3",
+                    hintText: "Enter Number",
+                  ),
+                  controller: t3,
+                  keyboardType: TextInputType.number,
+               
+                  ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Custom 4",
+                    hintText: "Enter Number",
+                  ),
+                  controller: t4,
+                  keyboardType: TextInputType.number,
+              
+                  ),
+                  new Text(
+                  "Ilosc Dawek",
+                  style: new TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple,
+                  ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Custom 5",
+                    hintText: "Enter Number",
+                  ),
+                  controller: t5,
+                  keyboardType: TextInputType.number,
+                  ),
+                RaisedButton(
+                  child: Text("Calculate"),
+                  onPressed: onPressed,
+                ),
+                new Text(
+                  "Dose: $score",
+                  style: new TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple,
+                  ),
+                ),
+              ],
+              ),
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+      );
+    }
+
 }
+
